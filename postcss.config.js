4499a9dfa9e84511d9b563f4565a219120d9f32e
Githubdiaries <151864736+Githubdiaries@users.noreply.github.com>
2026-02-13 00:37:46 +0530

    Updated postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,21 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {},
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    turbo: false, // critical for WASM / Bolt
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+    ],
   },
 };
+
+module.exports = nextConfig;
