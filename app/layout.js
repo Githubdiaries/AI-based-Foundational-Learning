bd689c2451cfb0d8cb398e0a53cbdad6baa83fda
Githubdiaries <151864736+Githubdiaries@users.noreply.github.com>
2026-02-25 02:58:16 +0530

    Fix title tag quotes in layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <title>GrammarPal – Catch 'Em All Grammar!</title>
+        <title>"GrammarPal – Catch 'Em All Grammar!"</title>
         <meta name="description" content="Learn English Grammar the Pokémon way!" />
         <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚡</text></svg>" />
       </head>
