3f76378e69700818f6f96c2043bc07647fd539f5
Githubdiaries <151864736+Githubdiaries@users.noreply.github.com>
2026-03-13 21:03:16 +0530

    Refactor RootLayout component in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-
+"use client"
 
 import './globals.css'
 import { ThemeProvider } from 'next-themes'
@@ -7,9 +7,10 @@ export const metadata = {
   title: "GrammarPal – Catch 'Em All Grammar!",
   description: "Learn English Grammar the Pokémon way!"
 }
-        <meta name="description" content="Learn English Grammar the Pokémon way!" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚡</text></svg>" />
-      </head>
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
